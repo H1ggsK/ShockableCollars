cc6845af682d5cc86bd89544205d5aec1a201193
h1ggsk <h1ggsk@outlook.com>
2026-02-23 09:12:10 -0800

    Added shock remote to creative menu

--- src/main/java/com/h1ggsk/shockablecollars/Shockablecollars.java
+++ src/main/java/com/h1ggsk/shockablecollars/Shockablecollars.java
@@ -1,12 +1,18 @@
 package com.h1ggsk.shockablecollars;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import com.h1ggsk.shockablecollars.registry.ModItems;
 import com.h1ggsk.shockablecollars.registry.ModSounds;
 
 public class Shockablecollars implements ModInitializer {
     public static final String MOD_ID = "shockablecollars";
+    private static final RegistryKey<ItemGroup> PLAYERCOLLARS_GROUP_KEY =
+            RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of("playercollars", "group"));
 
     public static Identifier id(String path) {
         return Identifier.of(MOD_ID, path);
@@ -16,5 +22,6 @@ public class Shockablecollars implements ModInitializer {
     public void onInitialize() {
         ModSounds.register();
         ModItems.register();
+        ItemGroupEvents.modifyEntriesEvent(PLAYERCOLLARS_GROUP_KEY).register(entries -> entries.add(ModItems.SHOCK_REMOTE));
     }
 }
